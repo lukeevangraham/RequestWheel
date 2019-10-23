6cb5e9fe26757f9ffd4184b4678fd01f6dd7f1aa
Cefalu Wave <luke.evan.graham@gmail.com>
2019-10-23 00:24:49 -0700

    intersting new response

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.use(routes);
 app.use((req, res, next) => {
   match({ routes, location: req.url }, (error, redirectLocation, renderProps) => {
     if (error) {
-      res.status(500).send(error.message)
+      res.status(500).alert(error.message)
     } else if (redirectLocation) {
       res.redirect(302, redirectLocation.pathname + redirectLocation.search)
     } else if(renderProps) {
