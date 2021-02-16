085030da4caa2851b2233e243a6e2502e849aa96
Cefalu Wave <luke.evan.graham@gmail.com>
2021-02-15 16:01:18 -0800

    getting the port number

---
+++ client/build/precache-manifest.c5b6335768397bc2fb195c15ff7f3899.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "135b64d911e3dab89e11",
+    "url": "/static/css/main.4e498eb4.chunk.css"
+  },
+  {
+    "revision": "135b64d911e3dab89e11",
+    "url": "/static/js/main.37d974f4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f2868fb59c4277e84517",
+    "url": "/static/css/2.d9982e85.chunk.css"
+  },
+  {
+    "revision": "f2868fb59c4277e84517",
+    "url": "/static/js/2.98d12d1f.chunk.js"
+  },
+  {
+    "revision": "b7ec03d3f2cd55289a274fe4e9546e26",
+    "url": "/index.html"
+  }
+];
