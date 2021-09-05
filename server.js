1ccacb8d68e0811d552cf89aa496e0d5e688365a
Cefalu Wave <luke.evan.graham@gmail.com>
2021-09-05 16:15:18 -0700

    using cookie-session instead of express-session

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-const session = require('express-session')
+const session = require('cookie-session')
 const passport = require('./passport')
 const path = require("path");
 const PORT = process.env.PORT || 3001;
